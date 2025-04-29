97da5db6b9ad05ba19b3a3c8744f36d690ae83c1
Artem Islamov <artem.islamov@eigenmethod.com>
2025-04-29 16:36:34 +0100

    fix send tansaction error. Add estimategas and max fee price

--- Services/RentalityBatchPriceUpdater.cs
+++ Services/RentalityBatchPriceUpdater.cs
@@ -1,16 +1,31 @@
-﻿using Nethereum.Web3;
+﻿using Nethereum.Hex.HexTypes;
+using Nethereum.Util;
+using Nethereum.Web3;
 using Rentality.PriceUpdater.Models;
 
 namespace Rentality.PriceUpdater.Services;
 
-internal class RentalityBatchPriceUpdater(Web3 web3, string batchUpdaterAbi, string batchUpdaterAddress, string walletPrivateKey)
+internal class RentalityBatchPriceUpdater(Web3 web3, string batchUpdaterAbi, string batchUpdaterAddress, string walletPrivateKey, ILogger<Worker> logger)
 {
     public async Task<string> UpdarePrices (List<OracleUpdate> updateRequest)
     {
 
         var rentalityBatchUpdaterContract = web3.Eth.GetContract(batchUpdaterAbi, batchUpdaterAddress);
         var updatePricesFunction = rentalityBatchUpdaterContract.GetFunction("updatePrices");
+        var account = new Nethereum.Web3.Accounts.Account(walletPrivateKey );
 
-        return await updatePricesFunction.SendTransactionAsync( walletPrivateKey, updateRequest);
+        var estimatedGas = await updatePricesFunction.EstimateGasAsync(account.Address, null, null, updateRequest);
+        var gasPrice = await web3.Eth.GasPrice.SendRequestAsync();
+        var maxPriorityFeePerGas = Web3.Convert.ToWei(5, UnitConversion.EthUnit.Mwei);
+        var maxFeePerGas = Web3.Convert.ToWei(25, UnitConversion.EthUnit.Mwei);
+        logger.LogInformation($"estimatedGas: {estimatedGas}, gasPrice: {gasPrice}");
+
+        return await updatePricesFunction.SendTransactionAsync(
+            from: account.Address, 
+            gas: new HexBigInteger(estimatedGas),
+            value: null, 
+            maxFeePerGas: new HexBigInteger(maxFeePerGas),
+            maxPriorityFeePerGas: new HexBigInteger(maxPriorityFeePerGas),
+            functionInput: updateRequest);
     }
 }
